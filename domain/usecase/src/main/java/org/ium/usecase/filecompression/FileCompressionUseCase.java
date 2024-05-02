29340dd90e047ee41fc7a190f905db47db7e88be
Cesar Burgos Rodriguez <cburgosro@iumotion.com>
2024-05-02 18:38:54 -0500

    3 --- CBR --- Update de versiones

--- domain/usecase/src/main/java/org/ium/usecase/filecompression/FileCompressionUseCase.java
+++ domain/usecase/src/main/java/org/ium/usecase/filecompression/FileCompressionUseCase.java
@@ -6,7 +6,9 @@ import org.ium.model.CompressedFile;
 import org.ium.model.FileToCompress;
 import org.ium.model.port.CompressionPort;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.nio.ByteBuffer;
 import java.util.Map;
 
 import static org.ium.model.CompressedFile.FileCompressionMetadata.NEW_BYTES;
@@ -17,7 +19,18 @@ public class FileCompressionUseCase implements CompressionPort {
     @Override
     public Mono<CompressedFile> compress(FileToCompress fileToCompress) {
         return Mono.just(fileToCompress)
-                .map(f -> Zstd.compress(f.byteBuffer(), f.compressionLevel()))
-                .map(bb -> new CompressedFile(bb, Map.of(OLD_BYTES, String.valueOf(fileToCompress.byteBuffer().length), NEW_BYTES, String.valueOf(bb.length))));
+                .map(f -> ByteBuffer.wrap(f.byteBuffer()))
+                .flatMap(byteBuffer -> compressFileReactively(byteBuffer, fileToCompress.compressionLevel()))
+                .map(bb -> new CompressedFile(bb.array(), Map.of(OLD_BYTES, String.valueOf(fileToCompress.byteBuffer().length),
+                        NEW_BYTES, String.valueOf(bb.array().length))));
+    }
+
+    private Mono<ByteBuffer> compressFileReactively(ByteBuffer byteBuffer, int compressionLevel) {
+        return Mono.fromCallable(() -> {
+            byte[] inputBytes = new byte[byteBuffer.remaining()];
+            byteBuffer.get(inputBytes);  // Copia los bytes de ByteBuffer a byte[]
+            byte[] compressedBytes = Zstd.compress(inputBytes, compressionLevel);
+            return ByteBuffer.wrap(compressedBytes);  // Envuelve el resultado comprimido en un ByteBuffer
+        }).subscribeOn(Schedulers.boundedElastic());  // Ejecuta en un hilo que permite operaciones bloqueantes
     }
 }
